9fec4238b654f9d43bf8e795d8cad22c1fdad641
Ruslan Yuldashev <rustik.yuldashev@yandex.ru>
2022-09-01 09:25:32 +0300

    добавить адаптив слайдера

--- js/main.js
+++ js/main.js
@@ -4,9 +4,21 @@ const owl = $('.owl-carousel');
 owl.owlCarousel({
   center: true,
   loop: true,
-  margin: 30,
   startPosition: 1,
   items: 3,
+  margin: 30,
+  responsive: {
+    850: {
+      items: 3,
+    },
+    1000: {
+      margin: 20,
+      items: 3
+    },
+    1200: {
+      margin: 30,
+    },
+  },
 });
 
 $('.slider__btn--prev').click(function () {
@@ -23,8 +35,8 @@ const navBtn = document.querySelector('.nav__toggle');
 const nav = document.querySelector('.nav');
 const menuIcon = document.querySelector('.menu-icon');
 
-navBtn.onclick = function(){
+navBtn.onclick = function () {
   nav.classList.toggle('nav--mobile');
   menuIcon.classList.toggle('menu-icon--active');
-}
-
+  document.body.classList.toggle('no-scroll');
+};
